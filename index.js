0f2cbbfa7973037721397481b76cf158660f2683
Mike Shine <michaelbshine@outlook.com>
2021-02-05 14:01:01 -0800

    added constructor function, myLibrary array, and functionality to display entries as library cards onscreen

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+const body = document.querySelector('body');
+
+const h1 = document.createElement('h1');
+h1.textContent = 'Shhh! Welcome to the Library.';
+body.appendChild(h1);
+
+const newBookButton = document.createElement('button');
+newBookButton.textContent = 'New book';
+body.appendChild(newBookButton);
+
+const newBookForm = document.createElement('form');
+newBookForm.classList.add('hideMe');
+
+const newBookLabel = document.createElement('label');
+newBookLabel.textContent =
+
+const libraryCardDisplay = document.createElement('div');
+libraryCardDisplay.classList.add('libraryCardDisplay');
+body.appendChild(libraryCardDisplay);
+
+
+
+
+/*  Library functionality */
+
+let myLibrary = [];
+
+
 function Book(title, author, pageCount, read) {
   this.title = title;
   this.author = author;
@@ -8,10 +36,78 @@ function Book(title, author, pageCount, read) {
     ? `${title} by ${author}, ${pageCount} pages, has been read.`
     : `${title} by ${author}, ${pageCount} pages, not read yet.`;
   }
-
-  ;
 }
 
-const book1 = new Book('algorithms', 'Adit Bhargava', 238, false);
+function addToLibrary(book) {
+  let newEntry = {
+    title: book.title,
+    author: book.author,
+    pageCount: book.pageCount,
+    read: book.read
+  }
+  myLibrary.push(newEntry);
+  displayLibrary();
+}
+const indexesOnDOM = [];
 
-console.table(book1.info());
+function displayLibrary() {
+  myLibrary.forEach((book, index) => {
+    if (indexesOnDOM.indexOf(index) !== -1) {
+      return;
+    } else {
+      let libraryCard = document.createElement('div');
+      libraryCard.classList.add('libraryCard');
+
+      let libraryCardTitle = document.createElement('div');
+      libraryCardTitle.textContent = `Title: ${book.title}`;
+      libraryCardTitle.classList.add('title');
+
+
+      let libraryCardAuthor = document.createElement('div');
+      libraryCardAuthor.textContent = `Author: ${book.author}`;
+      libraryCardAuthor.classList.add('author');
+
+
+      let libraryCardPageCount = document.createElement('div');
+      libraryCardPageCount.textContent = `Page count: ${book.pageCount}`;
+      libraryCardPageCount.classList.add('pageCount');
+
+
+      let libraryCardReadStatus = document.createElement('div');
+      let answer = null;
+      if (book.read) {
+        answer = 'Yes';
+      } else {
+        answer = 'No';
+      }
+      libraryCardReadStatus.textContent = `Have you read this book? ${answer}`;
+
+
+      libraryCard.appendChild(libraryCardTitle);
+      libraryCard.appendChild(libraryCardAuthor);
+      libraryCard.appendChild(libraryCardPageCount);
+      libraryCard.appendChild(libraryCardReadStatus);
+      indexesOnDOM.push(index);
+      libraryCardDisplay.appendChild(libraryCard);
+    }
+
+  })
+
+}
+
+// function clearLibrary(parentNode) {
+//   while (parentNode.firstChild) {
+//     parentNode.removeChild(parentNode.firstChild);
+//   }
+// }
+
+const book1 = new Book('algorithms', 'Adit Bhargava', 238, false);
+const book2 = new Book('What Color Is Your Parachute?', 'Richard Bolles', 309, false);
+const book3 = new Book('Kane and Abel', 'Jeffrey Archer', 512, true);
+
+addToLibrary(book1);
+addToLibrary(book2);
+addToLibrary(book3);
+
+
+console.table(myLibrary);
